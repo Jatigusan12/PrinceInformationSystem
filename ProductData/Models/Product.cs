1ffccbc4fc5b14cdfe9d7a0c0bc419d3b3bec11f
annabelle1999Belcina <annabelle.belcina@student.passerellesnumeriques.org>
2020-07-27 14:00:03 +0800

    first commit

---
+++ ProductData/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductData.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string ProductName { get; set; }
+        public int ProductQuantity { get; set; }
+        public double ProductPrice { get; set; }
+        public string ProductStatus { get; set; }
+        public DateTime DateAdded { get; set; }
+        public string PersonAdded{ get; set; }
+        public DateTime DateUpdated { get; set; }
+        public string PersonUpdated { get; set; }
+
+
+    }
+}
